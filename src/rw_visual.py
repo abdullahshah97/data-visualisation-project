f620062f379264252da728ba2700d964b06b5549
Abdullah Shah <abdullah-shah97@hotmail.com>
2020-02-15 14:45:19 +0000

    Increasing number of points to 50000 and emphasising start and end points

--- src/rw_visual.py
+++ src/rw_visual.py
@@ -5,9 +5,19 @@ from random_walk import RandomWalk
 #Make a random walk and plot the points
 #Keep making random walks whiile program is active
 while True:
-    rw = RandomWalk()
+    rw = RandomWalk(50000)
     rw.fill_walk()
-    plt.scatter(rw.x_values, rw.y_values, s=2)
+
+    plt.figure(figsize=(10,6))
+
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolor="none", s=1)
+    plt.scatter(0, 0, c='green', edgecolors='none', s = 50)
+    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s =50)
+
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
     plt.show()
 
     cont = input("Make another walk? (y/n): ")
