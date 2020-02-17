28c474a2911d313269e4efe83c84e52be432a1ea
Abdullah Shah <abdullah-shah97@hotmail.com>
2020-02-17 23:17:29 +0000

    List comprehensions implemented

--- src/different_dice.py
+++ src/different_dice.py
@@ -7,16 +7,20 @@ die_2 = Die(10)
 
 #Make some rolls, and store results in a list
 results = []
-for roll_num in range (50000):
-    result = die_1.roll() + die_2.roll()
-    results.append(result)
+results = [die_1.roll() + die_2.roll() for roll_num in range(50000)]
+
+#for roll_num in range (50000):
+#    result = die_1.roll() + die_2.roll()
+#    results.append(result)
 
 #Analyze the results
 frequencies = []
 max_result= die_1.num_sides + die_2.num_sides
-for value in range(2, max_result+1):
-    frequency = results.count(value)
-    frequencies.append(frequency)
+frequencies = [results.count(value) for value in range(2, max_result+1)]
+
+#for value in range(2, max_result+1):
+#    frequency = results.count(value)
+#    frequencies.append(frequency)
 
 print(frequencies)
 
