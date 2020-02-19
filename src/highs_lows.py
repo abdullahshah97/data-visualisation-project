ab20cd9a4e372fa1091a16802cd95d05a50fa655
Abdullah Shah <abdullah-shah97@hotmail.com>
2020-02-19 21:43:10 +0000

    Plot a graph for max temperatures

--- src/highs_lows.py
+++ src/highs_lows.py
@@ -1,19 +1,33 @@
 import csv
+from matplotlib import pyplot as plt
 
+#Weather file
 filename = 'sitka_weather_07-2014.csv'
 
-#CSV reader object and read line by line
+# CSV reader object and read line by line
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
 
-    #Retrieve column title and indexes
+    # Retrieve column title and indexes
     for index, column_header in enumerate(header_row):
         print(index, column_header)
 
-    #Retrieving max temps, first column of row has this value, convert to int so read by matplotlib
+    # Retrieving max temps, first column of row has this value, convert to int so read by matplotlib
     highs = []
     for row in reader:
-        highs.append(int(row[1]))
+        high = int(row[1])
+        highs.append(high)
 
     print(highs)
+
+# Plot the data
+fig = plt.figure(dpi=128, figsize=(10,6))
+plt.plot(highs, c='red')
+
+#Format plot
+plt.title("Daily High Temperatures, July 2014", fontsize = 24)
+plt.xlabel('', fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
