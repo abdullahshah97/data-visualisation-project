fdcb11739b49e256daa88069e050051d7f25c3bf
Abdullah Shah <abdullah-shah97@hotmail.com>
2020-02-19 20:54:45 +0000

    Read column and max temps from the csv

--- src/highs_lows.py
+++ src/highs_lows.py
@@ -1,7 +1,19 @@
 import csv
 
-filename = 'sitka_weather_07-14.csv'
+filename = 'sitka_weather_07-2014.csv'
+
+#CSV reader object and read line by line
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+
+    #Retrieve column title and indexes
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
+
+    #Retrieving max temps, first column of row has this value, convert to int so read by matplotlib
+    highs = []
+    for row in reader:
+        highs.append(int(row[1]))
+
+    print(highs)
