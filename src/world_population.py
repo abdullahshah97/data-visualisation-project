f339c0f46d5bc0b1e34c02657bbdaa640dbe31ee
Abdullah Shah <abdullah-shah97@hotmail.com>
2020-02-23 03:44:42 +0000

    render the grap of some countries in the americas usning pygal world map

--- src/world_population.py
+++ src/world_population.py
@@ -1,4 +1,5 @@
 import json
+import pygal
 from country_codes import get_country_code
 
 #Load the data into a list
@@ -7,6 +8,7 @@ with open(filename) as f:
     pop_data = json.load(f)
 
 #Print the 2010 population for each country
+cc_populations = {}
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
@@ -14,5 +16,12 @@ for pop_dict in pop_data:
         code = get_country_code(pop_dict['Country Name'])
         if code:
             print(code + ' : ' + str(population))
+            cc_populations[code] = population
         else:
             print('ERROR - ' +country_name)
+
+wm = pygal.maps.world.World()
+wm.title = 'World Population in 2010, by Country'
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
